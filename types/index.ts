5005315188e7f370b8c0ef267c148f6643a32d6f
Jawad Ali <jawadali66667@gmail.com>
2023-06-14 12:09:44 +0500

    Data Fetch using Rapid API done

--- types/index.ts
+++ types/index.ts
@@ -5,10 +5,28 @@ export interface CustomButtonProps {
     btnType?: "button" | "submit";
     containerStyles?: string;
     handleClick?: MouseEventHandler<HTMLButtonElement>;
+    textStyles?: string;
+    rightIcon?: string;
+    isDisabled?: boolean;
 }
 
 export interface SearchManufacturerProps {
     manufacturer: string,
     // Function that Takes manufacturer as string and returns nothing
     setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number,
+    class: string,
+    combination_mpg: number,
+    cylinders: number,
+    displacement: number,
+    drive: string,
+    fuel_type: string,
+    highway_mpg: number,
+    make: string,
+    model: string,
+    transmission: string,
+    year: number
 }
