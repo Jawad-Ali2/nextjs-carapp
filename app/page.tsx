8facaa02c0533fa82b55bff70d282aa8024cbe86
Jawad Ali <jawadali66667@gmail.com>
2023-06-15 13:09:03 +0500

    Done till the search logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,35 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { manufacturers } from '@/constants';
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }: any) {
 
-  const isDataEmpty = !Array.isArray(allCars) || allCars.length <1 || !allCars;
-  
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
+
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow-hidden">
       <Hero />
 
       <div className="mt-12 padding-x padding-y max-width" id='discover'>
         <div className="home__text-container">
-            <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
-            <p>Explore the cars you might like</p>
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
         </div>
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title = "fuel" />
-            <CustomFilter title = "year" />
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
           </div>
         </div>
 
@@ -36,7 +45,7 @@ export default async function Home() {
             <p>{allCars?.message}</p>
           </div>
         )}
-            
+
       </div>
     </main>
   )
--- utils/index.ts
+++ utils/index.ts
@@ -1,12 +1,16 @@
+import { FilterProps } from "@/types";
 
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+
+    const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
         'X-RapidAPI-Key': 'a4bbc6a440msh5edcf8cecb5aa97p1d3cdcjsnfe31607ae26c',
         'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
     }
 
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     });
 
